7a781eb8b20da1a8a59a5ca454880a3eead6cc12
samiyjface1 <delam101@gmail.com>
2026-01-19 18:57:49 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,12 @@
 
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("plenty-wheels").then(cache =>
+      cache.addAll([
+        "index.html",
+        "booking.html",
+        "services.html"
+      ])
+    )
+  );
+});
